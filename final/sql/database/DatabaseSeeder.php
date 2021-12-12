0113ca8d191f9963729ac1ad38711436256f8485
Tran (Cheryl) Vo <71985185+tv356@users.noreply.github.com>
2021-12-12 21:57:42 +0700

    Add files via upload

---
+++ final/sql/database/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        
+        DB::table('users')->insert([
+            'name'=>'admin',
+            'username'=>'admin',
+            'Image'=>'avatar.jpg',
+            'email'=>'admin@gmail.com',
+            'password'=>bcrypt('admin'),
+            'Role'=>1,
+            'Active'=>1
+        ]);
+    }
+}
